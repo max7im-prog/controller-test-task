84b23aac779d0c72c2c91019dd562dc556bdf37c
max7im <nogagorilli@gmail.com>
2025-12-17 12:57:04 +0300

    Wait() function for virtual serial

--- src/virtual_serial.cpp
+++ src/virtual_serial.cpp
@@ -6,6 +6,7 @@ void VirtualSerial::sendAtoB(const std::vector<uint8_t> &data) {
     std::lock_guard<std::mutex> lock(_mutexAToB);
     _queueAToB.push(data);
   }
+  _cvAToB.notify_one();
 }
 
 void VirtualSerial::sendBtoA(const std::vector<uint8_t> &data) {
@@ -13,6 +14,7 @@ void VirtualSerial::sendBtoA(const std::vector<uint8_t> &data) {
     std::lock_guard<std::mutex> lock(_mutexBToA);
     _queueBToA.push(data);
   }
+  _cvBToA.notify_one();
 }
 
 bool VirtualSerial::readA(std::vector<uint8_t> &out) {
@@ -37,4 +39,28 @@ bool VirtualSerial::readB(std::vector<uint8_t> &out) {
     _queueAToB.pop();
   }
   return true;
+}
+
+bool VirtualSerial::waitAToB() {
+  std::unique_lock<std::mutex> lock(_mutexAToB);
+  _cvBToA.wait(lock, [&]() { return _shutdown || !_queueAToB.empty(); });
+
+  return !_shutdown;
+}
+
+bool VirtualSerial::waitBToA() {
+  std::unique_lock<std::mutex> lock(_mutexBToA);
+  _cvBToA.wait(lock, [&]() { return _shutdown || !_queueBToA.empty(); });
+
+  return !_shutdown;
+}
+
+void VirtualSerial::shutdown() {
+  {
+    std::lock_guard<std::mutex> lockAToB{_mutexAToB};
+    std::lock_guard<std::mutex> lockBToA{_mutexBToA};
+    _shutdown = true;
+  }
+  _cvAToB.notify_all();
+  _cvBToA.notify_all();
 }
--- src/virtual_serial.h
+++ src/virtual_serial.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <condition_variable>
 #include <cstdint>
 #include <mutex>
 #include <queue>
@@ -13,10 +14,22 @@ public:
   bool readA(std::vector<uint8_t> &out);
   bool readB(std::vector<uint8_t> &out);
 
+  [[nodiscard]] bool waitAToB();
+  [[nodiscard]] bool waitBToA();
+
+  void shutdown();
+
+
 private:
   std::queue<std::vector<uint8_t>> _queueAToB;
   std::queue<std::vector<uint8_t>> _queueBToA;
 
   std::mutex _mutexAToB;
   std::mutex _mutexBToA;
+
+  std::condition_variable _cvAToB;
+  std::condition_variable _cvBToA;
+
+  bool _shutdown{false};
+
 };
